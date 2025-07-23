9c5683d1ea0d289415c33e3b509f26512e9bd6b8
JEWOO KWAK <65706899+jewookwak@users.noreply.github.com>
2025-07-23 14:38:30 +0900

    Add files via upload

---
+++ monitoring_camera_ws/build/aruco_marker_pkg/share/aruco_marker_pkg/hook/pythonpath_develop.ps1
@@ -0,0 +1,3 @@
+# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
+
+colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/addinnedu/monitoring_camera_ws/build/aruco_marker_pkg"
---
+++ monitoring_camera_ws/src/aruco_marker_pkg/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'aruco_marker_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='addinnedu',
+    maintainer_email='addinnedu@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'pose_publisher = aruco_marker_pkg.pose_publisher:main',
+            'marker_publisher_node = aruco_marker_pkg.aruco_detector:main',
+            'aruco_path_tracker = aruco_marker_pkg.aruco_path_tracker:main'
+        ],
+    },
+)
